da5d054918e1a8f0f3e8f48069d5e7da1fec3cde
Alvin Mustafa <alvinmustafa94@gmail.com>
2017-07-20 18:13:51 +0700

    Added ListTaskPage xaml and cs

---
+++ ToDoList/ToDoList/ToDoList/ListTaskPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ToDoList
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ListTaskPage : ContentPage
+	{
+        public List<ToDoItem> ToDoItems { get; set; }
+
+		public ListTaskPage (List<ToDoItem> items)
+		{
+            ToDoItems = items;
+            BindingContext = this;
+			InitializeComponent ();
+		}
+	}
+}
